6abb06086b53a4f2bc5d8d1d522139a2a06b60a6
wormjim <gcooled29@gmail.com>
2021-09-25 19:27:38 -0400

    Add Supportin Context For Board Machine

--- src/machine/BoardMachine/Board.Machine.ts
+++ src/machine/BoardMachine/Board.Machine.ts
@@ -3,6 +3,10 @@ import { createMachine } from 'xstate';
 export const BoardMachine = createMachine({
   id: 'board',
   initial: 'empty',
+  context: {
+    name: '',
+    items: [],
+  },
   states: {
     empty: {},
   },
