5d44111bd329ab1ba9b31a25ffb203d5fb38a11e
wormjim <gcooled29@gmail.com>
2021-09-25 19:26:44 -0400

    Support Layout For Root App

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import type { NextPage } from 'next';
+import BoardApp from '../src/components/BoardApp';
 import styles from '../styles/Root.module.css';
 
 const App: NextPage = () => {
-  return <div id='trello-root' className={styles.root}></div>;
+  return (
+    <div id={styles['trello-root']} className={styles['body-board-view']}>
+      <div id={styles.container}>
+        <div
+          style={{
+            height: '100%',
+            position: 'relative',
+            zIndex: 0,
+          }}>
+          <div id={styles['surface']} className={styles.surface}>
+            <main className={styles.main}>
+              <BoardApp />
+            </main>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
