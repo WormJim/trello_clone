127320c8449869887782a98c9e3f138ab2ab3ae8
wormjim <gcooled29@gmail.com>
2021-09-25 22:13:35 -0400

    Create State and Component For New List Item

---
+++ src/components/List/NewList.tsx
@@ -0,0 +1,97 @@
+import { useMachine } from '@xstate/react';
+import classNames from 'classnames';
+import React, { ChangeEvent, FormEvent, useCallback, useState } from 'react';
+import styles from '../../../styles/BoardApp.module.css';
+import { NewListMachine } from '../../machine/NewListMachine/NewListMachine';
+
+interface NewListProps {
+  onAdd: (name: string) => void;
+  onCancel?: () => void;
+}
+
+const NewList = ({ onAdd }: NewListProps) => {
+  const [idle, setIdle] = useState(true);
+  const [state, send] = useMachine(NewListMachine, {
+    actions: {
+      submit: (ctx) => onAdd(ctx.name),
+    },
+  });
+
+  const handleSubmit = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      send({ type: 'SUBMIT', input: state.context.name });
+    },
+    [send, state.context.name],
+  );
+
+  const onChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const input = event.target.value;
+      return send({ type: 'CHANGE', input });
+    },
+    [send],
+  );
+
+  return (
+    <div
+      className={classNames(
+        styles['add-list'],
+        styles['list'],
+        styles['list-wrapper'],
+        styles['mod-add'],
+        idle && styles['is-idle'],
+      )}>
+      <form onSubmit={handleSubmit}>
+        <a
+          className={classNames(styles['open-add-list'])}
+          href='#'
+          onClick={() => setIdle((curr) => !curr)}>
+          <span className={styles.placeholder}>
+            <span
+              className={classNames(styles['icon-sm'], styles['icon-add'])}
+            />
+            {'Add another list'}
+          </span>
+        </a>
+        <input
+          type='text'
+          name='name'
+          placeholder='Enter list title...'
+          autoComplete='off'
+          maxLength={512}
+          className={styles['list-name-input']}
+          onChange={onChange}
+        />
+
+        <div
+          className={classNames(
+            styles['list-add-controls'],
+            styles['u-clearfix'],
+          )}>
+          <input
+            className={classNames(
+              styles['nch-button'],
+              styles['nch-button--primary'],
+              styles['mod-list-add-button'],
+            )}
+            type='submit'
+            value='Add list'
+          />
+          <a
+            className={classNames(
+              styles['icon-lg'],
+              styles['icon-close'],
+              styles['dark-hover'],
+            )}
+            href='#'
+            aria-label='Cancel list editing'
+            onClick={() => setIdle((curr) => !curr)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default NewList;
---
+++ src/machine/NewListMachine/NewListMachine.ts
@@ -0,0 +1,40 @@
+import { assign, createMachine } from 'xstate';
+
+interface NewListContext {
+  name: string;
+}
+
+export type NewListEvent =
+  | { type: 'CHANGE'; input: string }
+  | { type: 'SUBMIT'; input: string };
+
+export const isNotEmpty = (context: NewListContext) => {
+  return context.name.length > 2;
+};
+
+export const NewListMachine = createMachine<NewListContext, NewListEvent>({
+  initial: 'active',
+  context: {
+    name: '',
+  },
+  states: {
+    active: {
+      on: {
+        CHANGE: {
+          actions: assign({
+            name: (ctx, event) => {
+              if (event.type === 'CHANGE') {
+                return event.input.trimLeft();
+              }
+              return ctx.name;
+            },
+          }),
+        },
+        SUBMIT: {
+          cond: isNotEmpty,
+          actions: 'submit',
+        },
+      },
+    },
+  },
+});
